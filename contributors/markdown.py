9382fe485920502d01bb65f95db134dd0fb2ece7
Harrison <harrison.cook@ecmwf.int>
2024-10-17 09:29:35 +0100

    Test multi repos

--- contributors/markdown.py
+++ contributors/markdown.py
@@ -191,6 +191,7 @@ def get_contributor_table(
         contribution_count = collaborator.contribution_count
         if repository:
             commit_urls = collaborator.commit_url
+        print(repository, type(repository))
         if organization or len(repository) > 1:
             # split the urls from the comma separated list and make them into markdown links
             commit_url_list = collaborator.commit_url.split(",")
